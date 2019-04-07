2a9940fc1240bb774e11e0f285e329eefb6bbf96
Dominika <dominikadolik6@gmail.com>
2019-04-07 20:50:16 +0200

    main.py: changes
    changes connected with new method

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from model import Model
 
+
 def run():
     model = Model()
     model.fillMessage()                                                                                                 # tworzenie msg
     model.createFrames()                                                                                                # dzielenie msg na ramki + bit parzystosci
-    model.printFrames(model.frames)
-    model.generateErrors()                                                                                              # przeslanie ramek
-    model.printFrames(model.framesWithErrors)
+    model.fillBadFrames()
+    model.printFrames(model.sentFrames)
+    model.stopAndWait()
+    # model.selectiveRepeat()
 
 if __name__ == '__main__':
     run()
